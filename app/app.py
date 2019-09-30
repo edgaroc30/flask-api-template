ff246c516b2a0c35e28b6c379c047822c6953a2b
Edgar <edgar.ochoa.c@gmail.com>
2019-09-30 15:20:11 +0200

    adding models

--- app/app.py
+++ app/app.py
@@ -1,20 +1,13 @@
 # app.py - a minimal flask api using flask_restful
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Api
+from model import main
 
 app = Flask(__name__)
 api = Api(app)
 
-class main(Resource):
-    def get(self):
-        return {'Hello'}
-
-class healthz(Resource):
-    def get(self):
-        return {'Status': 'Some body call for the exterminator?'}
-
-api.add_resource(healthz, '/healthz')
-api.add_resource(main, '/')
+api.add_resource(main.healthz, '/healthz')
+api.add_resource(main.main, '/')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
