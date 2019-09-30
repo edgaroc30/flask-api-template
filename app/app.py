3c14de446007e5df739dcb6587693146fae7feba
Edgar <edgar.ochoa.c@gmail.com>
2019-09-30 14:28:36 +0200

    Adding possible folder structure

--- app/app.py
+++ app/app.py
@@ -5,11 +5,16 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+class main(Resource):
     def get(self):
-        return {'hello': 'again works another world'}
+        return {'Hello'}
 
-api.add_resource(HelloWorld, '/')
+class healthz(Resource):
+    def get(self):
+        return {'Status': 'Some body call for the exterminator?'}
+
+api.add_resource(healthz, '/healthz')
+api.add_resource(main, '/')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
